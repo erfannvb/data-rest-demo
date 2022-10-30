db666af96043d1e9fdbebc85e434120dd1676207
Erfan <erfannavab@gmail.com>
2022-10-30 09:37:17 +0330

    - added findByFirstName query to repository

--- src/main/java/nvb/springframework/datarestdemo/repository/UserRepository.java
+++ src/main/java/nvb/springframework/datarestdemo/repository/UserRepository.java
@@ -2,8 +2,11 @@ package nvb.springframework.datarestdemo.repository;
 
 import nvb.springframework.datarestdemo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path = "members")
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    List<User> findByFirstName(String firstName);
+
 }
